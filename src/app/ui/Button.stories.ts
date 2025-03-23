cdb82aebd29a364db14f17a2ddfa2ee221e13c3d
Luna Yu <luna@monad.com>
2025-03-22 18:40:38 -0700

    add button component and story

---
+++ src/app/ui/Button.stories.ts
@@ -0,0 +1,99 @@
+import { StoryObj, Meta } from '@storybook/react';
+import Button from './Button';
+import { fn } from '@storybook/test';
+
+const meta = {
+    title: 'Button',
+    component: Button,
+    parameters: {
+        layout: 'centered',
+    },
+    args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const LargePrimary: Story = {
+    args: {
+        children: 'Button',
+        size: 'lg',
+    },
+};
+
+export const MediumPrimary: Story = {
+    args: {
+        children: 'Button',
+    },
+};
+
+export const SmallPrimary: Story = {
+    args: {
+        children: 'Button',
+        size: 'sm',
+    },
+};
+
+export const LargeSecondary: Story = {
+    args: {
+        children: 'Button',
+        size: 'lg',
+        variant: 'secondary',
+    },
+};
+
+export const MediumSecondary: Story = {
+    args: {
+        children: 'Button',
+        variant: 'secondary',
+    },
+};
+
+
+export const SmallSecondary: Story = {
+    args: {
+        children: 'Button',
+        size: 'sm',
+        variant: 'secondary',
+    },
+};
+
+export const Disabled: Story = {
+    args: {
+        children: 'Button',
+        disabled: true,
+    },
+};
+
+export const Link: Story = {
+    args: {
+        children: 'Button',
+        type: 'link',
+    },
+};
+
+export const LinkLarge: Story = {
+    args: {
+        children: 'Button',
+        type: 'link',
+        size: 'lg',
+    },
+};
+
+export const LinkSmall: Story = {
+    args: {
+        children: 'Button',
+        type: 'link',
+        size: 'sm',
+    },
+};
+
+export const LinkDisabled: Story = {
+    args: {
+        children: 'Button',
+        type: 'link',
+        disabled: true,
+    },
+};
---
+++ src/app/ui/button.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export type ButtonProps = {
+    children: React.ReactNode;
+    onClick?: () => void;
+    type?: 'button' | 'link';
+    variant?: 'primary' | 'secondary';
+    size?: 'sm' | 'md' | 'lg';
+    disabled?: boolean;
+    className?: string;
+};
+
+const Button: React.FC<ButtonProps> = ({
+    children,
+    onClick,
+    type = 'button',
+    variant = 'primary',
+    disabled = false,
+    size = 'md',
+}) => {
+    const baseStyles =
+        'rounded-md font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition-all cursor-pointer';
+    const variants = {
+        primary: 'bg-violet-600 text-white hover:bg-violet-400 focus:ring-violet-600',
+        secondary: 'text-violet-600 border-2 border-violet-600 hover:bg-zinc-100 focus:ring-amber-400',
+    };
+
+    const sizes = {
+        sm: 'px-2 py-1 text-sm',
+        md: 'px-4 py-2 text-base',
+        lg: 'px-6 py-3 text-lg',
+    };
+
+    const disabledStyles = 'opacity-50 cursor-not-allowed';
+
+    const computedClassName = `${baseStyles} ${variants[variant]} ${sizes[size]}
+         ${disabled ? disabledStyles : ''} `;
+
+    return type === 'link' ? (
+        <a
+            onClick={onClick}
+            className={`${baseStyles} ${disabled ? disabledStyles : ''} text-violet-600 hover:underline underline-offset-4 ease-in-out ${sizes[size]} `}
+            role="button"
+        >
+            {children}
+        </a>
+    ) : (
+        <button
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+            className={computedClassName}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
