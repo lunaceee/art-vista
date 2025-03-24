7219c6c369a3b3f3c83a69b9f2d569d23eef1e82
Luna Yu <luna@monad.com>
2025-03-24 12:33:47 -0700

    update next.config.ts to allow accessing external images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['www.artic.edu'],
+  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'www.artic.edu',
+      port: '',
+      pathname: '',
+      search: '',
+    },
+  ],
 };
 
 export default nextConfig;
