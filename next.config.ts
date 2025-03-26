87259c8416456cd333c3f76e17f84e69d02714b7
Luna Yu <luna@monad.com>
2025-03-25 23:22:04 -0700

    update image config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['www.artic.edu'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.artic.edu',
+        port: '',
+        pathname: '/iiif/**',
+      },
+    ],
   },
 };
 
