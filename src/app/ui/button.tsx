d4514dea1ab7d21f461f122ecfaa5d1a8d241f96
Luna Yu <luna@monad.com>
2025-03-26 07:50:17 -0700

    test

--- src/app/ui/button.tsx
+++ src/app/ui/button.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
 
+console.log('Button component loaded');
+
 export type ButtonProps = {
     children: React.ReactNode;
     onClick?: () => void;
