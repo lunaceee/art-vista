33ed6b4c8858f478701323e6126b269ec7762cc1
Luna Yu <luna@monad.com>
2025-03-25 22:43:47 -0700

    add global error

---
+++ src/app/global-error.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect } from 'react';
+import Button from '@/app/ui/button';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+
+export default function GlobalError({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    useEffect(() => {
+        // Optionally log the error to an error reporting service
+        console.error(error);
+    }, [error]);
+
+    return (
+        <html>
+            <body>
+                <main className="flex h-screen flex-col items-center justify-center gap-4">
+                    <ExclamationTriangleIcon className="w-10 text-zinc-400" />
+                    <h2 className="text-center">Something went wrong!</h2>
+                    <p className="text-center">
+                        {error.message}
+                    </p>
+                    <Button onClick={reset}>
+                        Try again
+                    </Button>
+                </main>
+            </body>
+        </html>
+    );
+}
